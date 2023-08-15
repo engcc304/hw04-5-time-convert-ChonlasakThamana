67169ded74197a995634b82e57ff4cb0a133da36
Chonlasak Tumna <137064083+ChonlasakThamana@users.noreply.github.com>
2023-08-15 14:50:30 +0700

    4--5

--- Homework/HW04/hw04-5-time-convert.cpp
+++ Homework/HW04/hw04-5-time-convert.cpp
@@ -12,4 +12,14 @@
             27
     Output:
         27 days = 2332800 seconds
-*/
+        
+ #include <stdio.h>
+ int main() {
+    int days;
+    printf("Input Days: ");
+    scanf("%d", &days);
+
+    int seconds = days * 86400;
+    printf("%d days = %d seconds\n", days, seconds);
+    return 0;
+}
